40dd8e38dbcc2afec2346030bbe5386ef7294f66
Jonathan Osorio <jonathan.vaouxoo.com>
2016-12-12 22:58:45 +0000

    [REF] openacademy: adding domain or and ilike

--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models
 
-class Parther(models.Model):
+class Partner(models.Model):
     _inherit = 'res.partner'
 
     instructor = fields.Boolean(default=False)
-    session_ids = fields.Many2many('openacademy.session', string="Session as intructor", readonly=True)
+    session_ids = fields.Many2many('openacademy.session', string="Session as attendee", readonly=True)
