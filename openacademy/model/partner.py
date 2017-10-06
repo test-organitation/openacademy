3b0868d8b3a50ba75666d63ad4535885aaf31857
JonathanOsAlc <jonathan@vauxoo.com>
2017-10-06 15:08:54 -0500

    [ADD] academy: add file test

--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp import fields, models
 
+
 class Partner(models.Model):
     _inherit = 'res.partner'
 
     instructor = fields.Boolean(default=False)
     session_ids = fields.Many2many('openacademy.session', string="Session as attendee", readonly=True)
+    test = fields.Char()
