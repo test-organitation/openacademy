11d23566be6ac097e403bf36ca3a81a589ff0522
JonathanOsAlc <jonathan@vauxoo.com>
2017-11-08 16:28:20 -0600

    test

--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -5,5 +5,7 @@ from openerp import fields, models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    instructor = fields.Boolean(default=False)
+    instructor = fields.Boolean(default=False, String="intructor patner")
     session_ids = fields.Many2many('openacademy.session', string="Session as attendee", readonly=True)
+    test2 = fields.Char()
+    test3 = fields.Char()
